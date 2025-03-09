0a994d14f3713deef787510223b3f26f0799aa65
Isma36 <99331924+Isma36@users.noreply.github.com>
2025-03-09 13:22:01 +0100

    Communication bewteen PC and NEO-6M is not working right now

--- ESP32 FIRMWARE update/src/main.cpp
+++ ESP32 FIRMWARE update/src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
-
 #include <HardwareSerial.h>
 
-HardwareSerial gpsSerial(1);  // UART1 para GPS
+HardwareSerial gpsSerial(0);  // UART1 para GPS
 
 void setup() {
     Serial.begin(115200);  // Comunicación con la PC por USB
-    gpsSerial.begin(9600, SERIAL_8N1, 12, 34);  // GPS en UART1 (TX=12, RX=34)
+    gpsSerial.begin(9600, SERIAL_8N1, 1, 3);  // TX=12, RX=15 (pines correctos en TTGO T-Beam)
 }
 
 void loop() {
     // Passthrough: Reenviar datos del GPS a la PC
     while (gpsSerial.available()) {
         Serial.write(gpsSerial.read());
+        Serial.print(" CPM | Geiger 2: ");
+        Serial.println(gpsSerial.read());
     }
 
     // Permitir enviar datos desde la PC al GPS (para flasheo en u-center)
